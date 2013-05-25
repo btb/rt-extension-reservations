26c7281911371a8f67cce0c03e8ba6baf5d66833
Bradley Bell <bradleyb@uw.edu>
2013-05-24 19:13:26 -0700

    untabify

--- lib/RT/Extension/Reservations.pm
+++ lib/RT/Extension/Reservations.pm
@@ -24,18 +24,18 @@ my $Orig_DeleteLink = \&DeleteLink;
 
     # Only applies to reservations lifecycle
     return $Orig_DeleteLink->( $self, %args )
-	unless $self->QueueObj->Lifecycle->Name eq 'reservations';
+        unless $self->QueueObj->Lifecycle->Name eq 'reservations';
 
     return $Orig_DeleteLink->( $self, %args )
-	if $self->QueueObj->Lifecycle->IsInitial( $self->Status );
+        if $self->QueueObj->Lifecycle->IsInitial( $self->Status );
 
     if ( $args{'Type'} eq 'RefersTo' && $args{'Target'} ) {
-	my $uri = RT::URI->new( $self->CurrentUser );
-	$uri->FromURI( $args{'Target'} );
-	my $obj = $uri->Resolver->Object;
+        my $uri = RT::URI->new( $self->CurrentUser );
+        $uri->FromURI( $args{'Target'} );
+        my $obj = $uri->Resolver->Object;
 
-	return ( 0, $self->loc('Can\'t delete asset from activated reservation') )
-	    if ( UNIVERSAL::isa($obj, 'RTx::AssetTracker::Asset') && $obj->id );
+        return ( 0, $self->loc('Can\'t delete asset from activated reservation') )
+            if ( UNIVERSAL::isa($obj, 'RTx::AssetTracker::Asset') && $obj->id );
     }
 
     return $Orig_DeleteLink->( $self, %args );
@@ -52,18 +52,18 @@ my $Orig_AddLink = \&AddLink;
 
     # Only applies to reservations lifecycle
     return $Orig_AddLink->( $self, %args )
-	unless $self->QueueObj->Lifecycle->Name eq 'reservations';
+        unless $self->QueueObj->Lifecycle->Name eq 'reservations';
 
     return $Orig_AddLink->( $self, %args )
-	if $self->QueueObj->Lifecycle->IsInitial( $self->Status );
+        if $self->QueueObj->Lifecycle->IsInitial( $self->Status );
 
     if ( $args{'Type'} eq 'RefersTo' && $args{'Target'} ) {
-	my $uri = RT::URI->new( $self->CurrentUser );
-	$uri->FromURI( $args{'Target'} );
-	my $obj = $uri->Resolver->Object;
+        my $uri = RT::URI->new( $self->CurrentUser );
+        $uri->FromURI( $args{'Target'} );
+        my $obj = $uri->Resolver->Object;
 
-	return ( 0, $self->loc('Can\'t add another asset to reservation') )
-	    if ( UNIVERSAL::isa($obj, 'RTx::AssetTracker::Asset') && $obj->id );
+        return ( 0, $self->loc('Can\'t add another asset to reservation') )
+            if ( UNIVERSAL::isa($obj, 'RTx::AssetTracker::Asset') && $obj->id );
     }
 
     return $Orig_AddLink->( $self, %args );
@@ -84,41 +84,41 @@ my $Orig_SetStatus = \&SetStatus;
 
     # Only applies to reservations lifecycle
     return $Orig_SetStatus->( $self, %args )
-	unless $self->QueueObj->Lifecycle->Name eq 'reservations';
+        unless $self->QueueObj->Lifecycle->Name eq 'reservations';
 
     RT::Logger->info($self->loc("Ticket [_1]: want to set status to [_2]", $self->Id, $args{'Status'}));
     # Are we trying to activate?
     return $Orig_SetStatus->( $self, %args )
-	unless $self->QueueObj->Lifecycle->IsActive( $args{'Status'} );
+        unless $self->QueueObj->Lifecycle->IsActive( $args{'Status'} );
 
 
     # Make sure we have valid start and end dates
     return ( 0, $self->loc('Can\'t be activated, \'Starts\' must be in the future.') )
-	unless $self->StartsObj->Unix > time;
+        unless $self->StartsObj->Unix > time;
 
     return ( 0, $self->loc('Can\'t be activated, \'Due\' must be after \'Starts\'.') )
-	unless $self->DueObj->Unix > $self->StartsObj->Unix;
+        unless $self->DueObj->Unix > $self->StartsObj->Unix;
 
 
     # Make sure we have a valid asset
     my ( $asset, $msg ) = $self->GetReservationAsset;
     return ( 0, $self->loc('Couldn\'t load asset: [_1]', $msg) )
-	unless $asset;
+        unless $asset;
 
 
     # Make sure linked asset is not already reserved
 
 
     my @args = (
-	$self->Id,
-	$asset->URI,
-	$self->StartsObj->ISO,
-	$self->DueObj->ISO,
-	$self->StartsObj->ISO, $self->DueObj->ISO,
-	$self->StartsObj->ISO, $self->DueObj->ISO,
-	$self->StartsObj->ISO, $self->StartsObj->ISO,
-	$self->DueObj->ISO, $self->DueObj->ISO,
-	join( " OR ", map { "Status = '$_'" } $self->QueueObj->Lifecycle->Valid('active') ),
+        $self->Id,
+        $asset->URI,
+        $self->StartsObj->ISO,
+        $self->DueObj->ISO,
+        $self->StartsObj->ISO, $self->DueObj->ISO,
+        $self->StartsObj->ISO, $self->DueObj->ISO,
+        $self->StartsObj->ISO, $self->StartsObj->ISO,
+        $self->DueObj->ISO, $self->DueObj->ISO,
+        join( " OR ", map { "Status = '$_'" } $self->QueueObj->Lifecycle->Valid('active') ),
     );
 
     my $sql = <<SQL;
@@ -141,8 +141,8 @@ SQL
     $tickets->FromSQL( $sql );
     $tickets->RedoSearch;
     if ( my $ticket = $tickets->First ) {
-	return ( 0, $self->loc('asset [_1] already reserved from [_2] to [_3] by ticket [_4]',
-			      $asset->Id, $ticket->StartsObj->AsString, $ticket->DueAsString, $ticket->Id) );
+        return ( 0, $self->loc('asset [_1] already reserved from [_2] to [_3] by ticket [_4]',
+                              $asset->Id, $ticket->StartsObj->AsString, $ticket->DueAsString, $ticket->Id) );
     }
 
     return $Orig_SetStatus->( $self, %args );
@@ -162,7 +162,7 @@ sub GetReservationAsset {
 
     # Only applies to reservations lifecycle
     return ( 0, $self->loc('Not a reservation ticket') )
-	unless $self->QueueObj->Lifecycle->Name eq 'reservations';
+        unless $self->QueueObj->Lifecycle->Name eq 'reservations';
 
     my $links = $self->RefersTo;
     $links->RedoSearch;
@@ -170,18 +170,18 @@ sub GetReservationAsset {
     my $Asset;
     RT::Logger->info( $self->loc("found [_1] links for ticket [_2]", $links->Count, $self->Id) );
     while ( my $link = $links->Next ) {
-	my $target = $link->TargetObj;
-	next unless ref( $target ) eq 'RTx::AssetTracker::Asset';
+        my $target = $link->TargetObj;
+        next unless ref( $target ) eq 'RTx::AssetTracker::Asset';
 
-	$Asset = $target;
-	$found++;
+        $Asset = $target;
+        $found++;
     }
 
     return ( 0, $self->loc('Too many referred assets') )
-	if $found > 1;
+        if $found > 1;
 
     return ( 0, $self->loc('No referred asset') )
-	if $found < 1;
+        if $found < 1;
 
     return ( $Asset, '' );
 }
@@ -194,17 +194,17 @@ package RTx::AssetTracker::Asset;
 sub CheckAvailability {
     my $self = shift;
     my %args = (
-	Starts => undef,
-	Due    => undef,
-	@_
+        Starts => undef,
+        Due    => undef,
+        @_
     );
 
     my $lifecycle = RT::Lifecycle->new( $self->CurrentUser );
     $lifecycle->Load( 'reservations' );
 
     my @values = (
-	$self->URI,
-	join( " OR ", map { "Status = '$_'" } $lifecycle->Valid('initial', 'active') ),
+        $self->URI,
+        join( " OR ", map { "Status = '$_'" } $lifecycle->Valid('initial', 'active') ),
     );
 
     my $sql = sprintf <<SQL, @values;
